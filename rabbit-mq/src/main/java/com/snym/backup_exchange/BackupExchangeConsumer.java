85fcff21d7b9281188b62954c1881704bc9dfde4
1972827581@qq.com <1972827581@qq.com>
2021-08-21 16:50:23 +0800

    rabbitmq 备份交换机代码示例

---
+++ rabbit-mq/src/main/java/com/snym/backup_exchange/BackupExchangeConsumer.java
@@ -0,0 +1,26 @@
+package com.snym.backup_exchange;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 消息消费者
+ *
+ * @author lzs
+ * @version 1.0
+ * @date 2021/8/21 1:18
+ */
+@Component
+@Slf4j
+public class BackupExchangeConsumer {
+
+    public static final String B_CONFIRM_QUEUE = "b.confirm_queue";
+
+    @RabbitListener(queues = B_CONFIRM_QUEUE)
+    public void receiveMsg(Message message){
+        String msg = new String(message.getBody());
+        log.info("消费者消费消息：{}",msg);
+    }
+}
--- rabbit-mq/src/main/java/com/snym/publish_confirm/SyncPublishConfirmProducer.java
+++ rabbit-mq/src/main/java/com/snym/publish_confirm/SyncPublishConfirmProducer.java
@@ -1,4 +1,4 @@
-package com.snym.publishconfirm;
+package com.snym.publish_confirm;
 
 import com.rabbitmq.client.Channel;
 import com.snym.utils.RabbitmqUtil;
